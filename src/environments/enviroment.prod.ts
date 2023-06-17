21e202ff52825e08968725de37f5a0fda4d29f34
Dmitry Makushyn <makushin.dima@gmail.com>
2023-06-17 16:32:14 +0300

    add enviroment

---
+++ src/environments/enviroment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  BACKEND_URL: `http://localhost:8000/`,
+  API_URL: `https://hotels4.p.rapidapi.com/properties/v2/`,
+  X_RapidAPI_Key: '41c8a73cc0msh36005253ddf9396p1a020ajsn71ab7eb472c5'
+};
