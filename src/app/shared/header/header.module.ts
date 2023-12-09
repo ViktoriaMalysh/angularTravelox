6f102f243f3fe7c07866d7a675ad96562e6047b8
Dmitry Makushyn <makushin.dima@gmail.com>
2023-12-09 09:41:45 +0200

    add header, minimal settings

--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -9,7 +13,12 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     HeaderComponent
--- src/app/shared/header/header/header.component.ts
+++ src/app/shared/header/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  selectedLang: string = 'ENG';
+  selectedCurrency: string = 'USD';
+  userName: string = 'Milady Dereck';
+  avatar: string = 'assets/avatar3.jpg';
 
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 }
